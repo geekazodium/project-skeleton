db0a61d1500b2c23442b424a85b0e5015ed5302a
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-02-26 20:51:45 +0800

    Ported State Machine System for Enemy Behavior

--- entities/minions/minion.gd
+++ entities/minions/minion.gd
@@ -1,23 +1,5 @@
 extends CharacterBody2D
 
-@export var target: Node2D = null;
-@export var nav_agent: NavigationAgent2D = null;
-@export var move_speed: float = 10;
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if nav_agent == null:
-		push_error("no nav agent set on node");
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if target == null:
-		return;
-	self.velocity = move_speed * self.get_nav_dir(target.global_position);
 	self.move_and_slide();
-	
-func get_nav_dir(target_pos: Vector2) -> Vector2:
-	self.nav_agent.target_position = target_pos;
-	var dir = self.nav_agent.get_next_path_position() - self.global_position;
-	return dir.normalized();
