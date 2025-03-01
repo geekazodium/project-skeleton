02c04daed120a3369f22cefa842ff817f6c5acad
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-03-01 17:52:25 +0800

    Cleaned up Merge Changes

--- scripts/event_bus.gd
+++ scripts/event_bus.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.position = self.get_viewport().get_mouse_position();
+	pass
