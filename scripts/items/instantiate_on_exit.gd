5058a425543982b0acb1e7eba61929b9ff360153
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-03-04 14:55:10 +0800

    Modified Event System and Added Enemy Death Event

--- scripts/items/instantiate_on_exit.gd
+++ scripts/items/instantiate_on_exit.gd
@@ -3,7 +3,8 @@ class_name InstantiateOnExit
 
 @export var scene: PackedScene = null;
 
-func _instantiate(to_replace: Node2D):
+func _instantiate(event: EntityDeathEvent):
 	var new_inst: Node2D = scene.instantiate();
+	var to_replace: Node2D = event.get_entity();
 	new_inst.global_position = to_replace.global_position;
 	to_replace.get_parent().add_child(new_inst);
