c47301d75d3565a1b64bc4adc23fb58d8e6732ab
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-03-15 22:58:54 +0800

    fix: tweaked thousand cuts and damage

--- scripts/progress/upgrades/combination_upgrades/thousand_cuts.gd
+++ scripts/progress/upgrades/combination_upgrades/thousand_cuts.gd
@@ -1,6 +1,7 @@
 extends UpgradeStrategy
 
-@export var chance_per_level: float = .2;
+@export var chance_per_level: float = .4;
+@export var rush_force: float = 700;
 
 func _ready():
 	EventBus.minion_dealt_damage.connect(self.on_minion_damage);
@@ -11,7 +12,7 @@ func on_minion_damage(event: EntityDealDamageEvent):
 	if randf() < bonus_chance:
 		damage_source.damage_cooldown_timer = 0;
 		var entity_body: EntityBody = damage_source.get_parent();
-		entity_body.apply_knockback((entity_body.global_position - event.get_entity().global_position).normalized() * -1000);
+		entity_body.apply_knockback((entity_body.global_position - event.get_entity().global_position).normalized() * -rush_force);
 
 func prerequisites_met(pool: Dictionary) -> bool:
 	return pool.get("Minion Health").level >= 1 && pool.get("Minion Attack Speed").level >= 1;
