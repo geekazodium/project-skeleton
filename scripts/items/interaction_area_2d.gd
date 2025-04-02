856a39fc3cd869bae7cf0f4fd7b8accc9174d705
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-04-02 20:54:26 +0800

    ci: clean up warnings and delete unused test button

--- scripts/items/interaction_area_2d.gd
+++ scripts/items/interaction_area_2d.gd
@@ -3,7 +3,7 @@ class_name InteractionArea2D
 
 @export var interact_action: String = "";
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed(self.interact_action):
 		var hit_area: InteractableArea2D = AttackArea.get_hit_nearest_area(self) as InteractableArea2D;
 		if hit_area == null:
