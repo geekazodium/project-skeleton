9f878a4d2eac3b2ae343c2e5e174e2e01212a564
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-04-02 20:52:24 +0800

    feature: saved item pickups! - feature: new test level

---
+++ scripts/items/saved_interactable_pickup.gd
@@ -0,0 +1,18 @@
+extends InteractableArea2D
+class_name SavedInteractablePickup
+
+@export var interacted_flag_name: String = "";
+
+func _ready() -> void:
+	if interacted_flag_name == "":
+		push_error("no picked up flag name set");
+		return;
+	
+	var picked_up: bool = SaveState.get_flag(self.interacted_flag_name);
+	if picked_up:
+		self.queue_free();
+
+func on_interact():
+	SaveState.set_flag(self.interacted_flag_name, true);
+	print("picked up item "+self.name);
+	self.queue_free();
