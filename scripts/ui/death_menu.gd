42a818ba61f20634a5e36411095347c895e5e02d
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-04-03 21:59:03 +0800

    feature: made game pause on death

--- scripts/ui/death_menu.gd
+++ scripts/ui/death_menu.gd
@@ -7,6 +7,8 @@ func _ready() -> void:
 
 func on_player_death(_event: EntityDeathEvent):
 	self.visible = true;
+	self.get_tree().paused = true;
 
 func _on_respawn_button_pressed() -> void:
 	self.get_tree().change_scene_to_file("res://scenes/game_scene.tscn");
+	self.get_tree().paused = false;
