6cc67c06395acff0bc1ef5da7451d7c21313d380
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-04-09 14:30:52 +0800

    feature: item pickup now has dialog

--- scripts/interactable/interactable_area_2d.gd
+++ scripts/interactable/interactable_area_2d.gd
@@ -1,6 +1,8 @@
 extends Area2D
 class_name InteractableArea2D
 
+@export var dialog: DialogueResource = null;
+
 func on_interact():
 	self.get_tree().paused = true;
 	var ballon = DialogueManager.show_dialogue_balloon(self.dialog);
