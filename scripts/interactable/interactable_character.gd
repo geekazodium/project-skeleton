d50b932b03a1b13dd6690b88d9eea5a7411fe903
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-05-03 19:06:08 +0800

    fix: interactable character only unpauses game when player does not attempt to trade upgrades.

--- scripts/interactable/interactable_character.gd
+++ scripts/interactable/interactable_character.gd
@@ -1,2 +1,17 @@
 extends InteractableArea2D
 class_name InteractableCharacter
+
+var unpause: bool = true;
+
+func _interact() -> void:
+	EventBus.trade_upgrades.connect(self.disable_unpause);
+	self.unpause = true;
+
+func disable_unpause(_unused) -> void:
+	self.unpause = false;
+
+func _dialog_ended() -> void:
+	EventBus.trade_upgrades.disconnect(self.disable_unpause);
+	if self.unpause:
+		self.get_tree().paused = false;
+	self.unpause = true;
