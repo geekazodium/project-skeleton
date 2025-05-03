57188b2b2b5789d7ba5867cae129ffeb5d1e4376
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-05-03 19:55:44 +0800

    fix: wrong connected event listener for interactable character.
    alt title: nightmare v2, wrong event electric boogalo

--- scripts/interactable/interactable_character.gd
+++ scripts/interactable/interactable_character.gd
@@ -4,14 +4,14 @@ class_name InteractableCharacter
 var unpause: bool = true;
 
 func _interact() -> void:
-	EventBus.trade_upgrades.connect(self.disable_unpause);
+	EventBus.level_ups_generated.connect(self.disable_unpause);
 	self.unpause = true;
 
 func disable_unpause(_unused) -> void:
 	self.unpause = false;
 
 func _dialog_ended() -> void:
-	EventBus.trade_upgrades.disconnect(self.disable_unpause);
+	EventBus.level_ups_generated.disconnect(self.disable_unpause);
 	if self.unpause:
 		self.get_tree().paused = false;
 	self.unpause = true;
