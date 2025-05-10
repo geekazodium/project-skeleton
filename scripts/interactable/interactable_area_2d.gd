81ae010194f49456115a9c5660b41e9a72ed4b39
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-05-10 13:08:44 +0800

    ci: added documentation for interactable area 2d

--- scripts/interactable/interactable_area_2d.gd
+++ scripts/interactable/interactable_area_2d.gd
@@ -1,20 +1,26 @@
 extends Area2D
 class_name InteractableArea2D
 
+## This script defines an abstract base class for dialog-based interactable objects in the game world.
+## It is intended to be extended by other scripts to implement specific interactable behaviors.
+## The node is designed to represent any object that can trigger dialog interactions.
+
 @export var dialog: DialogueResource = null;
 
-func on_interact():
+func on_interact() -> void:
+	print("Interaction started with dialog: ", self.dialog);
 	self.get_tree().paused = true;
 	var ballon = DialogueManager.show_dialogue_balloon(self.dialog);
 	ballon.process_mode = Node.PROCESS_MODE_ALWAYS;
 	ballon.tree_exiting.connect(self.on_dialog_ended);
 	self._interact();
 
-func on_dialog_ended():
+func on_dialog_ended() -> void:
+	print("Dialogue ended");
 	self._dialog_ended();
 
-func _interact():
+func _interact() -> void:
 	pass
 
-func _dialog_ended():
+func _dialog_ended() -> void:
 	pass
