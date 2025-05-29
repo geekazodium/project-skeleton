25238c78ea2b83f167135c4e9d0962ed17e29277
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-05-29 22:07:58 +0800

    feature: tank enemy and better waves

--- scripts/entities/spawners/enemy_wave.gd
+++ scripts/entities/spawners/enemy_wave.gd
@@ -2,26 +2,33 @@ extends Resource
 class_name EnemyWave
 
 @export var spawns: Array[PackedScene] = [];
+@export var spawn_amounts: Array[int] = [];
 
-@export var spawn_amount: int = 0;
-
-var spawn_count: int = 0;
+var spawns_left: Array[int] = [];
 
 func instantiate() -> Node2D:
-	return self.spawns.pick_random().instantiate() as Node2D;
+	var i: int = 0;
+	while i < self.spawns_left.size():
+		if self.spawns_left[i] > 0:
+			break;
+		i += 1;
+	self.spawns_left[i] -= 1;
+	return self.spawns[i].instantiate() as Node2D;
 
 func attempt_spawn(spawn_to: Node2D, spawn_position: Vector2) -> bool:
-	if self.spawn_amount <= self.spawn_count:
+	if self.spawining_done():
 		push_warning("attempted spawn on a completed spawner");
 		return false;
-	self.spawn_count += 1;
 	var new_instance = self.instantiate();
 	new_instance.position = spawn_position - spawn_to.global_position;
 	spawn_to.add_child(new_instance);
 	return true;
 
 func spawining_done() -> bool:
-	return self.spawn_amount <= self.spawn_count;
+	for l in spawns_left:
+		if l > 0:
+			return false;
+	return true;
 
 func reset():
-	self.spawn_count = 0;
+	self.spawns_left = self.spawn_amounts.duplicate();
