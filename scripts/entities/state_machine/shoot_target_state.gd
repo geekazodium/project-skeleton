f52fcf9b0ca28fd624d09f474c65dfd15c43d296
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-05-29 21:48:11 +0800

    feature: projectile enemies

--- scripts/entities/state_machine/shoot_target_state.gd
+++ scripts/entities/state_machine/shoot_target_state.gd
@@ -1,5 +1,5 @@
 extends Node2D
-class_name FollowTargetState
+class_name ShootTargetState
 
 ## Class is a state to be added to a parent state machine as a possible state,
 ## Node must have parent of state machine.
@@ -12,41 +12,44 @@ class_name FollowTargetState
 ## navigation agent 2d in that entity and set the `nav_agent` var to that.
 
 @export var target_tracker: TargetTracker = null;
-@export var character_body: EntityBody = null;
-@export var nav_agent: NavigationAgent2D = null;
+@export var cooldown_ended_state: StringName = "";
 @export var target_lost_state: StringName = "";
-@export var distance_tolerance: float = -1;
-@export var distance_tolerance_reached_state: StringName = "";
-@export var line_of_sight_raycast: RayCast2D = null;
+@export var cooldown_timer: Timer = null;
+@export var nav_agent: NavigationAgent2D = null;
+@export var character_body: EntityBody;
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if nav_agent == null:
-		push_error("no nav agent set on node");
+@export var projectile: PackedScene = null;
 
 func process(_delta: float) -> void:
 	pass;
 
+func cooldown_ended() -> void:
+	if StateMachine.is_active_state(self):
+		StateMachine.switch_state(self,cooldown_ended_state);
+
 # Called every physics tick. 'delta' is the elapsed time since the previous frame.
-func physics_process(delta: float) -> void:
+func physics_process(_delta: float) -> void:
 	if !self.target_tracker.has_target():
 		StateMachine.switch_state(self, target_lost_state);
 		return;
+	
 	var target = target_tracker.get_target();
-	if self.distance_tolerance > self.character_body.global_position.distance_to(target.global_position):
-		if self.has_line_of_sight(target):
-			StateMachine.switch_state(self, distance_tolerance_reached_state);
-			return;
-	character_body.move_in_direction(self.get_nav_dir(target.global_position), delta);
+	if self.cooldown_timer.is_stopped():
+		var relative_target_pos: Vector2 = target.global_position - self.character_body.global_position
+		self.spawn_projectile(atan2(relative_target_pos.y,relative_target_pos.x));
+		self.cooldown_timer.start();
+	else:
+		return;
+		#self.character_body.move_in_direction(self.get_nav_dir(target.global_position), delta);
 	
 func get_nav_dir(target_pos: Vector2) -> Vector2:
 	self.nav_agent.target_position = target_pos;
 	var dir = self.nav_agent.get_next_path_position() - character_body.global_position;
 	return dir.normalized();
 
-func has_line_of_sight(target: Node2D) -> bool:
-	if self.line_of_sight_raycast == null:
-		return true;
-	self.line_of_sight_raycast.target_position = self.line_of_sight_raycast.to_local(target.global_position);
-	self.line_of_sight_raycast.force_raycast_update();
-	return !self.line_of_sight_raycast.is_colliding();
+func spawn_projectile(projectile_rotation: float) -> void:
+	var projectile_inst: Node2D = self.projectile.instantiate() as Node2D;
+	EntityGroups.add_projectile(projectile_inst);
+	projectile_inst.global_position = self.global_position;
+	projectile_inst.global_rotation = projectile_rotation;
+	projectile_inst.reset_physics_interpolation();
