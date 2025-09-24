68a19f39465237b1b337422917ec317bd027bfe2
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-09-24 00:42:17 -0400

    feature: auto open file with code style violation

--- addons/no_spaghetti/checker/spaghetti_checker.gd
+++ addons/no_spaghetti/checker/spaghetti_checker.gd
@@ -2,27 +2,13 @@
 extends Node
 class_name SpaghettiChecker
 
-var keybind_pressed: bool = false;
-@export var buttons: Array[int] = [KEY_F,KEY_M];
-@export var lint_type: String = "gd";
 var safety_iter_limit: int = 128;
-
 @export var pastas: Array[Pasta] = [];
 
-func _process(delta: float) -> void:
-	if !Engine.is_editor_hint():
-		return;
-	
-	for keycode in buttons:
-		if self.keybind_pressed == Input.is_key_pressed(keycode):
-			return;
-	
-	self.keybind_pressed = !self.keybind_pressed;
-	
-	if !self.keybind_pressed:
-		return;
-	
-	self.check_program();
+signal lint_warnings_generated(path: String, text: String, results: Array[RegExMatch]);
+
+func add_pasta(pasta: Pasta) -> void:
+	self.pastas.append(pasta);
 
 func check_program() -> void:
 	for pasta in self.pastas:
@@ -30,15 +16,14 @@ func check_program() -> void:
 	
 	var count: int = self.check_folder("res://");
 	if count > 0:
-		print_rich("[color=yellow]warnings generated: "+String.num_int64(count));
+		SpaghettiLogger.rich("[color=yellow]warnings generated: "+String.num_int64(count));
 	else:
-		print_rich("[color=green]all clean!");
+		SpaghettiLogger.rich("[color=green]all clean!");
 	
-	print_rich("[color=pink]the linting is completed! please check the warnings and please try to address them");
+	SpaghettiLogger.rich("[color=pink]the linting is completed!");
 
 
 func check_folder(directory: String, layers: int = 64) -> int:
-	
 	var dir_access: DirAccess = DirAccess.open(directory);
 	var files: PackedStringArray = dir_access.get_files();
 	
@@ -76,26 +61,8 @@ func check_file(file_path_string: String) -> int:
 			text = file.get_as_text();
 			parsed = true;
 		pasta.search_all(text, results);
-	#
-	#results = results.filter(filter_self_ref);
 	
 	if results.size() > 0:
-		print_rich("\n[color=white]matches in: "+file.get_path());
-		
-	for r in results:
-		self.print_match(text,r);
-	
+		self.lint_warnings_generated.emit(file.get_path(), text, results);
 	file.close();
 	return results.size();
-#
-#func filter_self_ref(result: RegExMatch) -> bool:
-	#return result.get_string(1) != "self" && result.get_string(1) != "_self"
-
-func print_match(string: String,result: RegExMatch, context_size: int = 70):
-	var start = result.get_start();
-	var end = result.get_end();
-	print_rich(
-		"\n[color=gray]......"+string.substr(start-context_size, context_size)+
-		"[color=orange]"+result.get_string()+
-		"[color=gray]"+string.substr(end, context_size) +"......\n"
-		);
